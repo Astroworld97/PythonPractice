86b5f88a4cf127f5fcadcb0a81c1cc5a03871d89
iagonzal97 <iagonzal97@gmail.com>
2021-05-20 00:32:10 -0500

    added an input.py practice file to practice with inputs

---
+++ input.py
@@ -0,0 +1,8 @@
+name = input("Give me your name: ")
+print("Your name is " + name)
+
+age = input("Enter your age: ")
+age = int(age)
+age += 1
+age = str(age)
+print("Your age plus one is: " + age)
