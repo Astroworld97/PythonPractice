a6fb6afa48f4b5aec8d5bc144e73534358fd12b9
iagonzal97 <iagonzal97@gmail.com>
2021-05-20 00:49:50 -0500

    added mathWithStrings.py

---
+++ mathWithStrings.py
@@ -0,0 +1,9 @@
+#reference: https://www.practicepython.org/exercise/2014/01/29/01-character-input.html
+
+print("Were" + "wolf")
+print("Door" + "man")
+print("4" + "chan")
+print(str(4) + "chan")
+print(4 * "test")
+
+#note: division and subtraction do not work like this
