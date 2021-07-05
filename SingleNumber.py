2d01c543d25ccaefa93277f6f670a5689a7dc7ce
iagonzal97 <iagonzal97@gmail.com>
2021-07-04 21:42:10 -0400

    finished SingleNumber leetcode problem. Works successfully.

--- SingleNumber.py
+++ SingleNumber.py
@@ -1,18 +1,22 @@
-def singleNumber(self, nums: list) -> int:
-    d = dict()
-    for i in nums:
-        if i in d:
-            newQuantity = d.get(i) + 1
-            dict.update({i: newQuantity})
-        else:
-            dict.update({i: 0})
-    for i in range(0, len(d)):
-        num = d.get(i)
-        if num == 1:
-            return i
-    return 1000
+#source: https://leetcode.com/problems/single-number/submissions/
+class Solution:
 
+    def singleNumber(self, nums):
+        d = dict()
+        for i in nums:
+            if i in d:
+                newQuantity = d.get(i) + 1
+                d.update({i: newQuantity})
+            else:
+                d.update({i: 1})
+        for i in d.keys():
+            num = d.get(i)
+            if num == 1:
+                return i
+        return 1000
 
 if __name__ == '__main__':
     nums = [2, 2, 1]
-    singleNumber(nums)
+    s = Solution()
+    x = s.singleNumber(nums)
+    print(x)
